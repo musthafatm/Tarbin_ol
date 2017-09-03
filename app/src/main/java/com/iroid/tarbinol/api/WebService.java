73a64f5daa9151dcb073219f22bb03f8ced70fe8
musthafa <musthafatm3786@gmail.com>
2017-09-03 22:01:30 +0530

    commit..   :D

---
+++ app/src/main/java/com/iroid/tarbinol/api/WebService.java
@@ -0,0 +1,25 @@
+package com.iroid.tarbinol.api;
+
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * Created by Acer on 30-Aug-17.
+ */
+
+public interface WebService {
+
+    @FormUrlEncoded
+    @POST("/Salesexcutive/login")
+    Call<JsonObject> login(@Field("username") String userName, @Field("password")  String password);
+
+    @GET("/Salesexcutive/login")
+    Call<JsonObject> loginResponse(@Query("username") String userName, @Query("password")  String password);
+
+}
